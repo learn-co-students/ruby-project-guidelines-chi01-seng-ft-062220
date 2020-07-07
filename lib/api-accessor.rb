58d111829925b53ec631d01971953a09e8f30710
msguttman <msguttman@gmail.com>
2020-07-07 12:07:50 -0500

    file sstructure

---
+++ lib/api-accessor.rb
@@ -0,0 +1,25 @@
+require_relative '../config/environment.rb'
+
+def get_people
+    people = RestClient.get('https://ghibliapi.herokuapp.com/people')
+    people = JSON.parse(people)
+    people
+end
+
+def get_films
+    films = RestClient.get('https://ghibliapi.herokuapp.com/films')
+    films = JSON.parse(films)
+    films
+end
+
+def get_species
+    films = RestClient.get('https://ghibliapi.herokuapp.com/species')
+    films = JSON.parse(films)
+    films
+end
+
+def get_locations
+    films = RestClient.get('https://ghibliapi.herokuapp.com/locations')
+    films = JSON.parse(films)
+    films
+end
