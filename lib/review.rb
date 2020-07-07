8dc08b8793adb33ccf9cb9fc7a1e3fba2312f2c7
Eve Reichmann <eve.reichmann@gmail.com>
2020-07-07 14:06:56 -0500

    defined table keys

--- lib/review.rb
+++ lib/review.rb
@@ -1,6 +1,8 @@
 class Review < ActiveRecord::Base
     belongs_to :players
     belongs_to :games
+    
+    ##id: INTEGER PRIMARY KEY, player_id: INTEGER, game_id: INTEGER, player_review: TEXT, rating: INTEGER
 
     # def top_rated_game
     # end
