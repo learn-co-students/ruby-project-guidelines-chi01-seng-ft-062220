8837abe744c9bd7744a002333549d4677e0825d9
Eve Reichmann <eve.reichmann@gmail.com>
2020-07-07 12:00:40 -0500

    model assosiations, model setup

--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,13 @@
 class Player < ActiveRecord::Base
+    has_many :reviews
+    has_many :games, through: :reviews
 
+    # def write_or_update_review
+    # end
+    
+    # def delete_all_player_game
+    # end
+    
+    # def delete_game
+    # end
 end
--- lib/review.rb
+++ lib/review.rb
@@ -1,3 +1,7 @@
 class Review < ActiveRecord::Base
+    belongs_to :players
+    belongs_to :games
 
+    # def top_rated_game
+    # end
 end    
