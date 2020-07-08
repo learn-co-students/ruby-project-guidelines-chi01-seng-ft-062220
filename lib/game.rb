6a21790bf4ede248b022ba9d67f0786b97f0c2b4
haleyvferro <haleyvferro@gmail.com>
2020-07-07 20:46:51 -0500

    added basic game methods

--- lib/game.rb
+++ lib/game.rb
@@ -4,15 +4,28 @@ class Game < ActiveRecord::Base
 
     ##id: INTEGER PRIMARY KEY, name: TEXT, type: TEXT 
 
-
+    def self.list_by_type(type)
+        self.all.select do |game|
+            game.game_type == type
+        end
+    end
+            
     # def list_by_type(type)
         #pull type from game and match type 
         #map through array and print array of games  
-    # end    
+    # end   
+    
+    def self.ordered_by_rating
+        Review.all.sort { |r| r[:rating] }.reverse!
+    end
 
     # def ordered_by_rating
         #pull from reviews and sort ratings from best to worse
     # end 
+
+    def self.random_game
+        self.all.sample
+    end
 
     #def random_game
     #end
