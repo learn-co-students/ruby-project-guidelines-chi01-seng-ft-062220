1f70b5e5944e8d359e2d66b9813eab7243e6b8f2
Eve Reichmann <eve.reichmann@gmail.com>
2020-07-08 21:25:13 -0500

    command line intro, player_info, starting menu

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,34 +1,42 @@
+require_relative '../config/environment'
 $prompt = TTY::Prompt.new
     
-    def welcome 
+def game_intro
+   p "Welcome to" 
+   p'House Rules' 
+end
+
+def player_info
+    answer = $prompt.select("are you a", %w(Current_Player New_Player))
+        case
+            when answer == "Current_Player"
+                puts "What is your name?"
+                print ">"
+                name = gets.chomp
+                player = Player.all.find {|player| player.name == name}
+                puts "Hey #{player.name} nice to see you again!"
+            when answer == "New_Player"
+                puts "What is your name?"
+                print ">"
+                name = gets.chomp
+                player = Player.create(name: name)
+                puts "Hello #{player.name} thank you for joining us!"
+        end            
+end    
+
+def main_menu
         answer = $prompt.select("How can I help you?", %w(Reviews Games 
-        Player))
+        Suprise_me! Exit))
         case
         when answer == "Reviews"
             print ">"
         when answer == "Games"
             print ">"
-        when answer == "Player"
+        when answer == "Suprise_me!"
+            print ">"
+            puts Game.random_game.name
+        when answer == "Exit"
+            puts "Thanks for seeing you! Please come again!"
             print ">"
         end            
-    end
-
-    # def welcome
-    #     # puts $greeting.colorize(:color => :light_blue)
-    #     answer = $prompt.select("Welcome to BIG SPORTS CLI", "NEW USER", "EXISTING USER")
-    #         case
-    #         when answer == "NEW USER"
-    #             puts "Whats your name?"
-    #             print "> "
-    #             name = gets.chomp
-    #             $user = User.create(name: name)
-    #             puts "Your user ID is #{$user.id}, please remember this number!"
-    #         when answer == "EXISTING USER"
-    #             puts "Please enter your user ID"
-    #             print "> "
-    #             id = gets.chomp
-    #             $user = User.all.find{ |user| user.id == id.to_i }
-                
-    #             puts "Welcome back #{$user.name}"
-    #         end
-    # end
+end
